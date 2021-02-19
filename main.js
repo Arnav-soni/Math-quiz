07668def4fa4c0446031162d056dd4a570bb5131
Arnav-soni <73822785+Arnav-soni@users.noreply.github.com>
2021-02-19 15:46:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function login() {
+    window.location = "activity2.html";
+}
+
+function twitter() {
+    window.alert("Our site Is on Maintance so Twitter And Facebook Sign In Option Won't be Working  . We are sorry for that. Have A Nice Day (^◡^ ).");
+}
+function facebook() {
+    window.alert("Our site Is on Maintance so Twitter And Facebook Sign In Option Won't be Working  . We are sorry for that. Have A Nice Day (^◡^ ).");
+}
+    
